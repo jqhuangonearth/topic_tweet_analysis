daf13d112739f30770be12f14178e4d30c1643fe
JohnaLiu <zhijiaoliu@dhcp-10-201-76-49.tamulink.tamu.edu>
2013-11-19 15:56:39 -0600

    output state and count of topic

--- twitter_topic_analysis/tweets_topic_project/topic_zj.py
+++ twitter_topic_analysis/tweets_topic_project/topic_zj.py
@@ -1,11 +1,10 @@
 """
-This program contains lots of utility functions to analyze the topic
-for example, learn the distribution of topics, learn the temporal distribution
-of topics, and learn the geo distribution of topics
-@author: Bolun Huang
+
 """
 import nltk
 import cjson
+
+state_code = ["AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY"]
 
 class topic:
     def __init__(self):
@@ -15,11 +14,24 @@ class topic:
     def read_user_list(self, filename):
         f = open(filename, "r")
         for line in f:
-            data = cjson.decode(line)
-            data["geo_"]
+           data = cjson.decode(line)
+           location = data["geo_location"].split(",")
+           state = location[len(location) - 1].strip()
+           if not self.state.has_key(state):
+               self.state.update({state: 1})
+           else:
+                self.state[state] += 1
         f.close()
-        print len(self.user_dic)
-        print nltk.corpus.stopwords.words("english")
+        #print self.state
+        delete_list = []
+        for st in self.state:
+            if not st in state_code:
+                delete_list.append(st)
+        for dl in delete_list:
+            self.state.pop(dl)
+        print self.state
+        #print len(self.user_dic)
+        #print nltk.corpus.stopwords.words("english")
         
 def main():
     mytopic = topic()
@@ -27,4 +39,4 @@ def main():
     
     
 if __name__ == "__main__":
-PA'    main()
+    main()
